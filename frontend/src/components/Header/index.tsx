f69250256be8d8c801ce357a9db63edd3a2fbb08
Wanderson Noleto <onlywanderson@gmail.com>
2022-09-09 21:50:42 -0300

    week-day4 -r

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
 function Header() {
     return(
     <>
     <header>
         <div className="dsmeta-logo-container">
-        <img id='dsmeta-logo' src="assets/logo.svg" alt="Logo DSMeta"/>
+        <img id='dsmeta-logo' src={logo} alt="Logo DSMeta"/>
         <h1>DSMeta</h1>
         </div>
     </header>
